9f13bdaeadc657a77f2ce582d70393514b5fe901
ReginaChege <reginachegewairimu@gmail.com>
2023-02-15 13:44:02 +0300

    string

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main() {
+    fun main() {
+
+        var identity= "AkiraChix"
+        println(identity[0])
+        println(identity[3])
+        println(identity[4])
+
+        var x= "Hi my name is"
+        var name=" Philiph"
+        var y= " i am 50 years"
+        println(x + ""+ name + "" + y)
+
+        var naame= "Ryan"
+        var statement="its either thats me or i dont know who that is"
+        println("$naame its either thats me or i dont know who that is ")
+
+
+
+        var str="oranges"
+        println(str.length)
+
+    }
+}
